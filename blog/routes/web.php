1dcfa2cc0743889ec84e4b9ae37e15f80e0f6119
José Luis Jiménez <168377998+Z3R0codeX@users.noreply.github.com>
2025-08-27 15:27:20 -0700

    Modificacion de la DB
    creacion de la tabla post

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -6,6 +6,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get("/contacto", function(){
-    return view('contacto');
+Route::get("/contact", function(){
+    return view('contact');
+});
+
+Route::get("/post", function(){
+    return view('post');
+});
+
+Route::get("/about", function(){
+    return view('about');
 });
