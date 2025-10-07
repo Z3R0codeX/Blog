1dee062176e897438b6701e1fc1f3a4af998bd96
J Luis Jimenez <jliusjimenez@gmail.com>
2025-10-07 15:58:27 -0600

    login

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -34,3 +34,7 @@ Route::post("/users", [UsersController::class, "createUsers"]);
 
 
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
